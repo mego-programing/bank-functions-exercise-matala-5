d05f8562cdaa3ebe805865dd28b816b2677c1613
Elroy Anaki <162313628+Elroy-anaki@users.noreply.github.com>
2024-05-05 23:32:57 +0300

    Finish

--- bank_interface.py
+++ bank_interface.py
@@ -1,1 +1,49 @@
-# write your code here:
+def deposit(amount):
+    deposit_amount = float(input("How much money do you want to deposit? "))
+    amount += deposit_amount
+    return amount
+
+
+def withdraw(amount):
+    withdrawal_amount = float(input("How much money do you want to withdraw? "))
+    if withdrawal_amount > amount:
+        print("You can't do that!")
+        return amount
+
+    else:
+        amount -= withdrawal_amount
+        return amount
+
+
+def check_balance(amount):
+    return amount
+
+
+def main():
+    print(
+        f"Welcome to your bank account, your bank balance is: 0 $\nTo check your account click -- 1\nTo deposit click "
+        f"--"
+        f"2\nTo withdraw click -- 3\nTo exit at any stage click -- q")
+    bank_balance = 0
+    while True:
+        action = input("What do you want to do? ")
+
+        if action.lower() == "q":
+            print("Goodbye!")
+            quit()
+
+        elif action == "1":
+            print(f"Your balance is: {check_balance(bank_balance)}")
+
+        elif action == "2":
+            bank_balance = deposit(bank_balance)
+
+        elif action == "3":
+            bank_balance = withdraw(bank_balance)
+            
+        else:
+            print("Only 1, 2, 3, or q")
+
+
+if __name__ == "__main__":
+    main()
