cb347f2221b629896a95f249acff66368248177a
Elroy Anaki <162313628+Elroy-anaki@users.noreply.github.com>
2024-05-06 09:21:22 +0300

    Finish

--- bank_interface.py
+++ bank_interface.py
@@ -1,3 +1,6 @@
+bank_balance = 0
+
+
 def deposit(amount):
     deposit_amount = float(input("How much money do you want to deposit? "))
     amount += deposit_amount
@@ -19,31 +22,28 @@ def check_balance(amount):
     return amount
 
 
-def main():
-    print(
-        f"Welcome to your bank account, your bank balance is: 0 $\nTo check your account click -- 1\nTo deposit click "
-        f"--"
-        f"2\nTo withdraw click -- 3\nTo exit at any stage click -- q")
-    bank_balance = 0
-    while True:
-        action = input("What do you want to do? ")
+print(
+    f"Welcome to your bank account, your bank balance is: 0 $\nTo check your account click -- 1\nTo deposit click "
+    f"--"
+    f"2\nTo withdraw click -- 3\nTo exit at any stage click -- q")
+while True:
+    action = input("What do you want to do? ")
 
-        if action.lower() == "q":
-            print("Goodbye!")
-            quit()
+    if action.lower() == "q":
+        print("Goodbye!")
+        quit()
 
-        elif action == "1":
-            print(f"Your balance is: {check_balance(bank_balance)}")
+    elif action == "1":
+        print(f"Your balance is: {check_balance(bank_balance)}")
 
-        elif action == "2":
-            bank_balance = deposit(bank_balance)
+    elif action == "2":
+        bank_balance = deposit(bank_balance)
 
-        elif action == "3":
-            bank_balance = withdraw(bank_balance)
+    elif action == "3":
+        bank_balance = withdraw(bank_balance)
             
-        else:
-            print("Only 1, 2, 3, or q")
+    else:
+        print("Only 1, 2, 3, or q")
 
 
-if __name__ == "__main__":
-    main()
+
